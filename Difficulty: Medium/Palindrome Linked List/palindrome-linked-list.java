48b691f2829418ace473e0fa0bf2979b261a2569
ShivamGupta01032004 <shivamkumarcii999@gmail.com>
2025-12-31 11:33:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    boolean isPalindrome(Node head) {
+        if (head == null || head.next == null)
+            return true;
+
+        // Step 1: Find middle
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        Node secondHalf = reverseList(slow);
+        Node firstHalf = head;
+
+        // Step 3: Compare
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    Node reverseList(Node head) {
+        Node prev = null;
+
+        while (head != null) {
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
